2cc407b9f6863df0788db05dad76947c9534083e
Kevin <kevincarr632@gmail.com>
2020-06-25 10:17:55 -0500

    completed in_order_print function

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -54,15 +54,15 @@ class BSTNode:
         if target < self.value:
             if self.left is None:
                 return False
-            found = self.left.contains(target)
+            return self.left.contains(target)
         # same as above just this time we check if current value is greater than the target and
         # if so we go to the right instead
-        if target >= self.value:
+        if target > self.value:
             if self.right is None:
                 return False
-            found = self.right.contains(target)
+            return self.right.contains(target)
 
-        return found
+        
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -76,8 +76,6 @@ class BSTNode:
             return self.value
         else:
             return self.right.get_max()
-
-
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
@@ -93,11 +91,24 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+       
+        # go all the way to the left then print that node
+        if self.left:
+            self.left.in_order_print(node)
+        print(self.value)
+        # then go right and print that node
+        if self.right:
+            self.right.in_order_print(node)
+      
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
+        # create a queue for nodes 
+        # add first node to the queue
+        # while queue is not empty remove the first node
+            # print the removed node
+            # add all children into the queue
         pass
 
     # Print the value of every node, starting with the given node,
